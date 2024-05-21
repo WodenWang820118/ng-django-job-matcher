31d1069fa18e74bd1880afde49b2003add760a8a
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-05-21 19:15:45 +0800

    refactor: remove and adjust elements

--- ng-frontend/src/app/app.component.ts
+++ ng-frontend/src/app/app.component.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { AppService } from './app.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterOutlet],
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  template: `<router-outlet></router-outlet>`,
+  styles: [''],
 })
 export class AppComponent {
   title = 'ng-frontend';
-  constructor(private appService: AppService) {
-    this.appService.getBlogPosts().subscribe((data) => {
-      console.log(data);
-    });
-  }
 }
