9503887d92c0ad7208fff97a25d48bab95a299cc
Guan-Xin Wang <guanxinwang0118@gmail.com>
2024-05-28 22:29:22 +0800

    feat: mardown resume view

--- ng-frontend/src/app/shared/components/card-resume/card-resume.component.component.ts
+++ ng-frontend/src/app/shared/components/card-resume/card-resume.component.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MarkdownModule],
   selector: 'app-card-resume',
   template: `<mat-card appearance="outlined">
     <mat-card-header>
       <mat-card-title>Resume</mat-card-title>
     </mat-card-header>
     <mat-card-content>
-      <p>{{ resume }}</p>
+      <markdown [data]="resume"></markdown>
     </mat-card-content>
   </mat-card>`,
 })
